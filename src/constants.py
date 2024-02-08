579c763ac61b57000bb002e317c139be3bcb91ab
Udit Kumar Chatterjee <quantumudit@gmail.com>
2024-02-08 14:49:59 +0530

    python environment setup

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+"""
+This module contains various constants that can be used in python scripts.
+It contains the following:
+
+Constants:
+- CONFIGS: The path to the configuration file.
+"""
+
+from os.path import normpath
+
+# Configuration file paths
+CONFIGS = normpath("conf/configs.yaml")
